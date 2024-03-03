47f2c3efcf30d64de8f65d2f3d6d199554cf1f62
fialajiri <jirik.fiala@post.cz>
2024-03-03 09:21:35 +0100

    open registration 2024

--- components/ui-elements/banner.js
+++ components/ui-elements/banner.js
@@ -1,7 +1,7 @@
 const Banner = () => {
   return (
     <div className="sliding-banner">
-      <h1 className="heading-secondary banner-text">25.11.2023 v Muzeu</h1>
+      <h1 className="heading-secondary banner-text">22.6.2024 v Šanovském parku</h1>
     </div>
   );
 };
