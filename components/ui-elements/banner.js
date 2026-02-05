bcc165b51ddd84679a790470c688cd45d5667f2e
fialajiri <jirik.fiala@post.cz>
2026-02-05 16:21:25 +0100

    feat: banner fix

--- components/ui-elements/banner.js
+++ components/ui-elements/banner.js
@@ -1,7 +1,7 @@
 const Banner = () => {
   return (
     <div className="sliding-banner">
-      <h1 className="heading-secondary banner-text">20. 6. 2026 Živé Teplice v muzeu Šanovském parku u Mušle</h1>
+      <h1 className="heading-secondary banner-text">20. 6. 2026 Živé Teplice v Šanovském parku u Mušle</h1>
     </div>
   );
 };
