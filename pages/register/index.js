8c52d5a102443a8fdf1a7854d0b4eaf1f8edf34d
fialajiri <jirik.fiala@post.cz>
2026-02-05 15:55:15 +0100

    fix: merge fix

--- pages/register/index.js
+++ pages/register/index.js
@@ -2,7 +2,7 @@ import { Fragment } from "react";
 import Head from "next/head";
 import Register from "../../components/pages/register/register";
 import RegistrationClosed from "../../components/pages/register/registration-closed";
-import RegistrationClosed from "../../components/pages/register/registration-closed";
+
 
 const RegisterPage = (props) => {
   return (
